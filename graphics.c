821b7fcc44b2a3724437dda4099c27a2a97e7bbe
03stevensmi <mankymania12@gmail.com>
2023-02-26 14:03:42 +0000

    Update graphics.c

--- graphics.c
+++ graphics.c
@@ -38,6 +38,9 @@ const pngs_t png_logos[] = {
 	{ "multichoice",    _png_multichoice,    IMG_POS_TR, sizeof(_png_multichoice) },
 	{ "skyone",         _png_skyone,         IMG_POS_TL, sizeof(_png_skyone) },
 	{ "sky",            _png_sky,            IMG_POS_BR, sizeof(_png_sky) },
+	{ "spacetoon",      _png_spacetoon,      IMG_POS_TR, sizeof(_png_spacetoon) },
+	{ "viva",           _png_viva,           IMG_POS_TR, sizeof(_png_viva) },
+	{ "foxkids",        _png_foxkids,        IMG_POS_TL, sizeof(_png_foxkids) },
 	{ NULL,             NULL,                0,          0 }
 };
 
